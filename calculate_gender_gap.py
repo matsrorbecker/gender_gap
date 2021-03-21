974008be3c6bccf7849eab02286e13096f59faef
Mats Rörbecker <mats@rorbecker.com>
2021-03-21 07:58:17 +0100

    Better handling of sparse data

--- calculate_gender_gap.py
+++ calculate_gender_gap.py
@@ -8,9 +8,6 @@ filename = sys.argv[1]
 output = []
 output.append(['month', 'gender_gap1', 'gender_gap2'])
 
-def to_float(str):
-    return 0 if str == '..' else float(str)
-
 with open(filename, newline='') as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -25,12 +22,15 @@ with open(filename, newline='') as f:
         gender_gap1 = 0 # sum of all differences
         gender_gap2 = 0 # sum of all significant (at 95% level) differences
         for i in range(1, NUMBER_OF_PARTIES + 1):
-            support_from_men = to_float(row[i])
-            support_from_women = to_float(row[i + NUMBER_OF_PARTIES])
+            # calculate difference only if there are estimates for both sexes
+            if row[i] == '..' or row[i + NUMBER_OF_PARTIES] == '..':
+                continue
+            support_from_men = float(row[i])
+            support_from_women = float(row[i + NUMBER_OF_PARTIES])
             difference = abs(support_from_men - support_from_women)
             gender_gap1 += difference
-            error_margin_men = to_float(row[i + NUMBER_OF_PARTIES * 2])
-            error_margin_women = to_float(row[i + NUMBER_OF_PARTIES * 3])
+            error_margin_men = float(row[i + NUMBER_OF_PARTIES * 2])
+            error_margin_women = float(row[i + NUMBER_OF_PARTIES * 3])
             if difference - error_margin_men - error_margin_women > 0:
                 gender_gap2 += difference
         output.append([row[0], "{:.1f}".format(gender_gap1), "{:.1f}".format(gender_gap2)])
