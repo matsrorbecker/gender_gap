cfed58d5ae240b9625d253d44eaf052dc378eef2
Mats Rörbecker <mats@rorbecker.com>
2021-03-28 08:21:43 +0200

    Show both measures and legend

--- draw_chart.py
+++ draw_chart.py
@@ -6,15 +6,27 @@ import matplotlib.pyplot as plt
 filename = sys.argv[1]
 
 x_data = []
-y_data = []
+y_data1 = []
+y_data2 = []
 
 with open(filename, newline='') as f:
     reader = csv.reader(f)
-    next(reader) # skip header row
+    header_row = next(reader)
+    x_label = header_row[0]
+    y_label1 = header_row[1]
+    y_label2 = header_row[2]
     for row in reader:
         x_data.append(row[0])
-        y_data.append(float(row[2]))
+        y_data1.append(float(row[1]))
+        y_data2.append(float(row[2]))
 
-plt.plot(x_data, y_data)
+data = {}
+data[x_label] = x_data
+data[y_label1] = y_data1
+data[y_label2] = y_data2
+
+plt.plot(x_label, y_label1, y_label2, data=data)
+plt.ylim(bottom=-1)
 plt.xticks(rotation='vertical')
+plt.legend([y_label1, y_label2])
 plt.show()
